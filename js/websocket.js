32906e728780cb70e88123d004c8cb9de62c7ae1
orcaprog <infoayoub77.com>
2024-05-23 08:35:20 +0100

    yese

---
+++ js/websocket.js
@@ -0,0 +1,8 @@
+let page = document.querySelector(".page")
+
+
+let request = document.createElement("div")
+
+request.className = "message-request  fixed-bottom "
+request.textContent = "x send you raquest :)"
+page.appendChild(request)
